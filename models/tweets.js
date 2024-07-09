390a61428839518c726d710fe46d1f15a0fcdc85
Ines L <ines.latrache@gmail.com>
2024-07-09 15:07:47 +0200

    save

--- models/tweets.js
+++ models/tweets.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 const tweetSchema = mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
   message: String,
+  like: {
+    type: Number,
+    default: 0,
+  },
+  likeBy: [{ type: String }],
 });
 
 const Tweet = mongoose.model("tweets", tweetSchema);
--- routes/tweet.js
+++ routes/tweet.js
@@ -16,8 +16,8 @@ router.post("/newTweet", (req, res) => {
       user: data._id,
       message: req.body.message,
     });
-    newTweet.save().then(() => {
-      res.json({ results: true });
+    newTweet.save().then((data) => {
+      res.json({ results: data });
     });
   });
 });
@@ -39,6 +39,45 @@ router.delete("/deleteTweet/:id", (req, res) => {
   Tweet.deleteOne({ _id: req.params.id }).then(() =>
     res.json({ result: true })
   );
+});
+
+router.post("/incrementLike", async (req, res) => {
+  // if (!checkUser(req.body, ["token"])) {
+  //   res.json({ results: false, error: "Missing or empty fields" });
+  //   return;
+  // }
+
+  try {
+    const userToken = await User.findOne({ token: req.body.token });
+    if (!userToken) {
+      res.json({ result: false, error: "User not found" });
+      return;
+    }
+
+    const tweet = await Tweet.findOne({ _id: req.body.id });
+    if (!tweet) {
+      res.json({ result: false, error: "Tweet not found" });
+      return;
+    }
+
+    if (tweet.likeBy.includes(userToken.token)) {
+      const updatedTweet = await Tweet.findOneAndUpdate(
+        { _id: req.body.id },
+        { $pull: { likeBy: userToken.token } },
+        { new: true }
+      );
+      res.json({ result: updatedTweet });
+    } else {
+      const updatedTweet = await Tweet.findOneAndUpdate(
+        { _id: req.body.id },
+        { $push: { likeBy: userToken.token } },
+        { new: true }
+      );
+      res.json({ result: updatedTweet });
+    }
+  } catch (err) {
+    res.json({ result: err.message });
+  }
 });
 
 module.exports = router;
